bef17407aeac8e1703da46d7269b215e7f6798e7
vlad <vladkalyuzhnyu@gmail.com>
2021-10-22 15:03:22 +0300

    send worker

---
+++ custom/src/main/java/androidovshchik/tg/sms/SendWorker.kt
@@ -0,0 +1,111 @@
+package androidovshchik.tg.sms
+
+import android.content.Context
+import android.provider.Telephony.Sms
+import androidovshchik.tg.sms.ext.longBgToast
+import androidovshchik.tg.sms.local.Preferences
+import androidx.work.*
+import androidx.work.WorkInfo.State
+import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.model.request.ParseMode
+import com.pengrad.telegrambot.request.SendMessage
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZoneOffset
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+class SendWorker(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
+
+    override fun doWork(): Result = with(applicationContext) {
+        val lastSmsId = inputData.getLong(PARAM_SMS_ID, -1L)
+        Timber.d("Input lastSmsId is $lastSmsId")
+        var hasErrors = false
+        val preferences = Preferences(applicationContext)
+        val token = preferences.botToken?.trim()
+        if (token.isNullOrBlank()) {
+            Timber.w("Bot token is not set")
+            longBgToast("Не задан токен бота")
+            return@with Result.failure()
+        }
+        val chats = db.chatDao().selectAll()
+        Timber.d(chats.toString())
+        val minSmsId = (chats.firstOrNull()?.nextMsgId ?: lastSmsId).toString()
+        val bot = TelegramBot(token)
+        for (chat in chats) {
+            if (!cursor.moveToFirst()) {
+                break
+            }
+            do {
+                try {
+                    val smsId = cursor.getLong(cursor.getColumnIndexOrThrow(Sms._ID))
+                    Timber.d("Processing of smsId $smsId")
+                    if (chat.nextMsgId > smsId) {
+                        Timber.d("Skipping nextSmsId is ${chat.nextMsgId}")
+                        continue
+                    }
+                    val address = cursor.getString(cursor.getColumnIndexOrThrow(Sms.ADDRESS))
+                    val body = cursor.getString(cursor.getColumnIndexOrThrow(Sms.BODY))
+                    val timestamp = cursor.getLong(cursor.getColumnIndexOrThrow(Sms.DATE))
+                    val date = ZonedDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneOffset.UTC)
+                        .withZoneSameInstant(ZoneId.systemDefault())
+                    val message = SendMessage(chat.id, """
+                            $address
+                            ```$body```
+                            ${date.format(formatter)}
+                        """.trimIndent())
+                    val response = bot.execute(message.parseMode(ParseMode.Markdown))
+                    if (response.isOk) {
+                        db.chatDao().update(chat.also {
+                            it.nextMsgId = smsId + 1
+                        })
+                        Timber.d("Updating $chat")
+                    }
+                } catch (e: Throwable) {
+                    Timber.e(e)
+                    hasErrors = true
+                    break
+                }
+            } while (cursor.moveToNext())
+        }
+        return if (hasErrors) Result.retry() else Result.success()
+    }
+
+    companion object {
+
+        private const val NAME = "Send"
+
+        private const val PARAM_SMS_ID = "sms_id"
+
+        private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss (Z)")
+
+        private val lock = Any()
+
+        fun launch(context: Context, lastSmsId: Long?) = synchronized(lock) {
+            val request = OneTimeWorkRequestBuilder<SendWorker>()
+                .setInputData(
+                    Data.Builder()
+                        .putLong(PARAM_SMS_ID, lastSmsId ?: -1L)
+                        .build()
+                )
+                .setBackoffCriteria(BackoffPolicy.LINEAR, 10, TimeUnit.SECONDS)
+                .build()
+            with(WorkManager.getInstance(context)) {
+                val count = getWorkInfosForUniqueWork(NAME).get()
+                    .filter { it.state == State.ENQUEUED || it.state == State.RUNNING }
+                    .size
+                if (count < 2) {
+                    enqueueUniqueWork(NAME, ExistingWorkPolicy.APPEND_OR_REPLACE, request)
+                }
+            }
+        }
+
+        fun cancel(context: Context) {
+            WorkManager.getInstance(context).apply {
+                cancelUniqueWork(NAME)
+            }
+        }
+    }
+}
--- custom/src/main/java/androidovshchik/tg/sms/SettingsFragment.kt
+++ custom/src/main/java/androidovshchik/tg/sms/SettingsFragment.kt
@@ -3,8 +3,10 @@ package androidovshchik.tg.sms
 import android.os.Bundle
 import androidovshchik.tg.sms.local.Preferences
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -16,14 +18,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
         authCode?.setOnPreferenceChangeListener { _, newValue ->
             val value = newValue.toString().trim()
             if (value.isNotBlank() && preferences.authCode != value) {
+                val context = requireContext()
                 preferences.authCode = value
                 authCode.text = value
                 doAsync {
                     db.chatDao().deleteAll()
-                    MainWorker.cancel(requireContext())
+                    SendWorker.cancel(context)
                 }
             }
             false
+        }
+        val updInbox = preferenceManager.findPreference<Preference>("update_inbox")
+        updInbox?.setOnPreferenceClickListener {
+            val context = requireContext()
+            context.toast("Подождите...")
+            UpdateWorker.launch(requireContext())
+            true
         }
     }
 }
